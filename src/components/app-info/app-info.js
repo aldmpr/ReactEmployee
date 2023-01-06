998bbc6fc68aed1b86f94a537365b32ca5e5ad88
Alex <aldmpr@gmail.com>
2023-01-06 01:21:02 +0100

    react

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании Miratech</h1>
-            <h3>Общее число сотрудников:</h3>
-            <h3>Премию получат: </h3>
+            <h1>Employee accounting at "Company Name"</h1>
+            <h3>Total number of employees: {employees}</h3>
+            <h3>The award will be given to: {increased}</h3>
         </div>
     )
 }
